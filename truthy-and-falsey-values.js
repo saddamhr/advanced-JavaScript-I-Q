a84a3e2e097959c43761ef998d7d05fa9fdc8e2d
saddamhr <44530098+saddamhr@users.noreply.github.com>
2020-03-03 23:40:17 +0600

    true false values

---
+++ truthy-and-falsey-values.js
@@ -0,0 +1,28 @@
+/**
+ * Truthy values in JavaScript
+ * '0'
+ * ' '
+ * let name = []; empty array
+ * let name = {}; empty object
+ * true
+ */
+
+ /**
+ * Falsy values in JavaScript
+ * 0; Zero
+ * ''
+ * null
+ * undefined
+ * NaN; not a number
+ * false
+ */
+
+// const age = 0;
+// const name = '0';
+let name = null;
+
+if(name) {
+    console.log('Condition is true');
+} else {
+console.log('Condition is false');
+}
